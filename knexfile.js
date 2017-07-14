8498a87e9d8f1dbedfa958ee11187f3a1f045c3b
Jordan Doig <jordandoig@live.com>
2017-07-14 14:26:20 -0600

    Changed favicon

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/DandDatabase'
+    connection: 'postgres://localhost/shatterwood-inn'
   },
 
   production: {
