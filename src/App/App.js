22a22f63a63f71833303c3b9ca83c60d6a47a7f4
Jordan Doig <jordandoig@live.com>
2017-07-17 13:26:12 -0600

    Making fake class page

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,95 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
-import ClassList from '../Class/class';
+
+const classList = [
+  {
+    id: 1,
+    name: 'Barbarian',
+  },
+  {
+    id: 2,
+    name: 'Bard',
+  },
+  {
+    id: 3,
+    name: 'Cleric',
+  },
+  {
+    id: 4,
+    name: 'Druid',
+  },
+  {
+    id: 5,
+    name: 'Fighter',
+  },
+  {
+    id: 6,
+    name: 'Monk',
+  },
+  {
+    id: 7,
+    name: 'Paladin',
+  },
+  {
+    id: 8,
+    name: 'Ranger',
+  },
+  {
+    id: 9,
+    name: 'Rogue',
+  },
+  {
+    id: 10,
+    name: 'Sorceror',
+  },
+  {
+    id: 11,
+    name: 'Warlock',
+  },
+  {
+    id: 12,
+    name: 'Wizard',
+  }
+];
+
+// const raceList = [
+//   {
+//     id: 1,
+//     name: 'Dwarf'
+//   },
+//   {
+//     id: 2,
+//     name: 'Elf'
+//   }
+// ];
+//
+// const subraceList = [
+//   {
+//     id: 1,
+//     name: 'Dwarf'
+//   },
+//   {
+//     id: 2,
+//     name: 'Elf'
+//   }
+// ];
 
 class App extends Component {
   constructor () {
     super();
+    this.state = {
+      progress: [],
+      display: []
+    }
   }
 
   componentDidMount () {
+    this.getClasses();
+  }
 
+  getClasses () {
+    this.setState({display: classList});
   }
 
   render() {
@@ -28,12 +108,32 @@ class App extends Component {
         </header>
         <main className="App-main">
           <section className="Progress-bar">
-            <p>Progress Goes Here</p>
+            <header>
+              <h2>Progress</h2>
+            </header>
+            {
+              this.state.progress.map((item, index) => {
+                return (
+                  <h3 key={index} className="Progress-item">{item.title}: {item.desc}</h3>
+                )
+              })
+            }
           </section>
           <section className="Main-content">
             <h2>Classes</h2>
             <section>
-              <ClassList />
+              {
+                this.state.display.map((item, index) => {
+                  return (
+                    <section key={index} className="Display-section">
+                        <button>Choose Me!</button>
+                      <section className="Display-content">
+                        <h3>{item.name}</h3>
+                      </section>
+                    </section>
+                  )
+                })
+              }
             </section>
           </section>
         </main>
--- src/Class/class.js
+++ src/Class/class.js
@@ -2,39 +2,51 @@ import React, { Component } from 'react';
 
 const classList = [
   {
+    id: 1,
     name: 'Barbarian',
   },
   {
+    id: 2,
     name: 'Bard',
   },
   {
+    id: 3,
     name: 'Cleric',
   },
   {
+    id: 4,
     name: 'Druid',
   },
   {
+    id: 5,
     name: 'Fighter',
   },
   {
+    id: 6,
     name: 'Monk',
   },
   {
+    id: 7,
     name: 'Paladin',
   },
   {
+    id: 8,
     name: 'Ranger',
   },
   {
+    id: 9,
     name: 'Rogue',
   },
   {
+    id: 10,
     name: 'Sorceror',
   },
   {
+    id: 11,
     name: 'Warlock',
   },
   {
+    id: 12,
     name: 'Wizard',
   }
 ];
@@ -48,7 +60,7 @@ class Classes extends Component {
   }
 
   componentDidMount () {
-    this.getClasses();
+    this.getClasses()
   }
 
   getClasses () {
@@ -59,8 +71,15 @@ class Classes extends Component {
     return (
       <div className="Class-list">
         {
-          this.state.charClasses.map((cl) => {
-            return <h3>{cl.name}</h3>
+          this.state.charClasses.map((cl, index) => {
+            return (
+              <section className="Class-section">
+                <h3 key={index.toString()}>{cl.name}</h3>
+                <section>
+
+                </section>
+              </section>
+            )
           })
         }
       </div>
