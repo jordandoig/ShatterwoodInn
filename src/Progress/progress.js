0b002f46492bde0e40e635063d31820256fc84c1
Jordan Doig <jordandoig@live.com>
2017-07-19 15:10:07 -0600

    Added Progress Component

---
+++ src/Progress/progress.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Progress extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      choices: {}
+    }
+  }
+
+  render () {
+    return (
+      <div className="Progress">
+
+      </div>
+    )
+  }
+}
+
+export default Progress;
