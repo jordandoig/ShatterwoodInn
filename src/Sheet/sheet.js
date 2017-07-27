192c1b3f8ae0b50dded62882cf1e28df7f0758a2
Jordan Doig <jordandoig@live.com>
2017-07-27 07:24:49 -0600

    Working on creating a pdf

---
+++ src/Sheet/sheet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import { Page, Text, View, Document, StyleSheet } from '@react-pdf/core';
+import './sheet.css';
+
+class Sheet extends Component {
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default Sheet;
